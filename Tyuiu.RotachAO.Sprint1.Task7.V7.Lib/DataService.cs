ca851d03424191a14c5e29c8319129c96ab10578
RotachAO@outlook.com <RotachAO@outlook.com>
2024-10-05 14:06:27 +0500

    Task7

--- Tyuiu.RotachAO.Sprint1.Task7.V7.Lib/DataService.cs
+++ Tyuiu.RotachAO.Sprint1.Task7.V7.Lib/DataService.cs
@@ -7,7 +7,7 @@ public class DataService : ISprint1Task7V7
     public double Calculate(double x, double y)
     {
         double res = (Math.Cos(x) / (Math.PI - 2 * (Math.Pow(y, x)))) + 16 * x * Math.Cos(x * y) - 2;
-        return res;
+        return Math.Round(res,3);
     }
 }
 
